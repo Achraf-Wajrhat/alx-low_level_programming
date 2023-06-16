9367934f75c7bfc7b4d7e631fde0da083fadb6e1
Achraf Wajrhat <wajachraf@gmail.com>
2023-06-16 06:29:08 -0700

    See if the last digit greater or less or equal to a num and print it

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - See if the last digit greater than 5, less than 6, or equal to 0
+ * main - Prints the last digit of a randomly generated number and whether it is greater than 5, less than 6, or 0.
  *
- * Return : Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -12,10 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	}
+	}										}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
@@ -23,9 +25,8 @@ int main(void)
 	else
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	}											
+	}
 	return (0);
 
 }
-
 
