bd6f1076d2f7a48e6c8127cfda88804b0c719cab
Achraf Wajrhat <wajachraf@gmail.com>
2023-06-16 05:24:14 -0700

    Create a program that assign a random num

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,18 @@
-#include<stdlib.h>
-#include<time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * If : if statement
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
