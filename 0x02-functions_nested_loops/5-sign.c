b94201fd7a68f032c176b7abf150894fac4cfc78
Achraf Wajrhat <wajachraf@gmail.com>
2023-06-20 07:54:11 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,7 +14,7 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	} 
-	else if (n == 0)i
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
@@ -24,5 +24,4 @@ int print_sign(int n)
 		_putchar(45);
 		return (-1);
 	}
-		
 }
